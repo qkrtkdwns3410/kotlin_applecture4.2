f48a9846e145c9267cce63f6274ba2724b0156d9
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-04-06 19:15:37 +0900

    4.6 강의 프라그먼트

---
+++ app/src/main/java/com/example/myapp/Android/Fragment1.kt
@@ -0,0 +1,27 @@
+package com.example.myapp.Android
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class Fragment1:Fragment(){
+    override fun onAttach(context: Context) {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/myapp/Android/InternetView.kt
@@ -0,0 +1,47 @@
+package com.example.myapp.Android
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.myapp.R
+import kotlinx.android.synthetic.main.activity_internet_view.*
+
+
+class InternetView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_internet_view)
+
+
+        btn1.setOnClickListener {
+            val address = editText1.text.toString()
+            val intent =Intent(Intent.ACTION_VIEW, Uri.parse(address))
+            startActivity(intent)
+        }
+
+        editText1.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("edit","beforeTextChanged : "+s)
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("edit","onTextChanged : "+s)
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("edit","afterTextChanged : "+s)
+            }
+        })
+    }
+}
