447f853da5881434917aea9571bc5324aa49a365
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-04-06 22:15:03 +0900

    4.5 프라그먼트

--- app/src/main/java/com/example/myapp/Android/Fragment1.kt
+++ app/src/main/java/com/example/myapp/Android/Fragment1.kt
@@ -2,18 +2,22 @@ package com.example.myapp.Android
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapp.R
 
 
 class Fragment1:Fragment(){
     override fun onAttach(context: Context) {
-
+        Log.d("Life Cycle","onAttach")
+        super.onAttach(context)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Life Cycle","onCreate")
         super.onCreate(savedInstanceState)
     }
 
@@ -22,6 +26,51 @@ class Fragment1:Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        // 프라그먼트가 인터페이스를 처음으로 그릴 때 호출된다.
+        // inflater >> 뷰를 그려주는 역할
+        //container >> 부모뷰
+        Log.d("Life Cycle","onCreateView")
+        return inflater.inflate(R.layout.fragment_one,container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("Life Cycle","onViewCreated")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d("Life Cycle","onActivityCreated")
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d("Life Cycle","onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("Life Cycle","onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("Life Cycle","onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("Life Cycle","onStop")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d("Life Cycle","onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDetach() {
+        Log.d("Life Cycle","onDetach")
+        super.onDetach()
     }
 }
--- app/src/main/java/com/example/myapp/Android/FragmentActivity.kt
+++ app/src/main/java/com/example/myapp/Android/FragmentActivity.kt
@@ -2,11 +2,61 @@ package com.example.myapp.Android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
 import com.example.myapp.R
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        Log.d("Life Cycle", "onCreate")
+
+        button.setOnClickListener{
+             //프라그먼트를 동적으로 작동하는 방법
+            val fragment1 :Fragment1 = Fragment1()
+            val fragmentManager: FragmentManager = supportFragmentManager
+
+            // Transaction
+            // 작업의 단위  >>시작과 끝이 있다
+            val fragmentTransaction = fragmentManager.beginTransaction()
+        }
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Life Cycle", "onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Life Cycle", "onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Life Cycle", "onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Life Cycle", "onStop")
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Life Cycle", "onDestroy")
+
+    }
+
 }
